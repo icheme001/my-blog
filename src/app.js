f5a55d59e97ba65aab4ad01e3d838f1082f57e04
Icheme Genesis Ojochegbe <123704115+icheme001@users.noreply.github.com>
2025-12-25 20:47:47 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://YOUR_FRONTEND_DOMAIN.vercel.app"
+      "https://paaublog.vercel.app/"
     ],
     credentials: true
   })
